3a6b58a9ff1c620322ad0e432a810e102c220314
vPankaja <vpankaja42@gmail.com>
2023-03-26 04:35:08 +0530

    final

--- client/components/screen/TimeTable/ScheduleList.js
+++ client/components/screen/TimeTable/ScheduleList.js
@@ -6,12 +6,16 @@ import {
   TouchableOpacity,
   FlatList,
   TextInput,
-  ImageBackground,
+  Image,
+  ScrollView,
+  Dimensions,
 } from "react-native";
 import Toast from "react-native-toast-message";
 import { useNavigation } from "@react-navigation/native";
 import { db } from "../../firebase-config/firebase-config";
 import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { Card } from "react-native-paper";
+const { width, height } = Dimensions.get("window");
 
 export default function ScheduleList() {
   const [getData, setGetData] = useState("");
@@ -53,124 +57,141 @@ export default function ScheduleList() {
         item.selectedDay.toLowerCase().includes(searchTerm.toLowerCase()) ||
         item.Module.toLowerCase().includes(searchTerm.toLowerCase())
     );
-  
+
     const sorted = filtered.sort((a, b) => {
       const aTime = parseInt(a.selectedTime.split(":").join(""));
       const bTime = parseInt(b.selectedTime.split(":").join(""));
       return aTime - bTime;
     });
-  
+
     setFilteredData(sorted);
   };
-  
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text
-          style={{
-            color: "#0D0140",
-            fontWeight: "bold",
-            fontSize: 30,
-            marginTop: 20,
-            textAlign: "center",
-          }}
-        >
-          Class Schedule List
-        </Text>
-        <br />
-        <View style={{ flexDirection: "row", marginHorizontal: 10 }}>
-          <TextInput
-            style={styles.searchInput}
-            onChangeText={(text) => setSearchTerm(text)}
-            value={searchTerm}
-            placeholder="Search by day or module"
-            placeholderTextColor="#666"
-          />
-          <TouchableOpacity
-            style={styles.searchButton}
-            onPress={() => handleSearch()}
-            underlayColor="#0084fffa"
-          >
-            <Text style={styles.searchButtonText}>Search</Text>
-          </TouchableOpacity>
-        </View>
+    <ScrollView>
+    <View style={{ flex: 1,  backgroundColor: "#c8e3d0",paddingBottom:400 }}>
+      <Image
+        style={{
+          width: "30%",
+          height: "2%",
+          alignItems: "center",
+          marginTop: 10,
+          marginLeft: 260,
+          marginBottom: -40,
+          zIndex: 1,
+          borderRadius: 50, // set the borderRadius property to 50 (half of the image width)
+          overflow: "hidden",
+        }}
+        source={{
+          uri: "https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1626336719708x872173843502919200%2FDate_time_convertor.gif?w=&h=&auto=compress&dpr=1&fit=max",
+        }}
+      />
+      
+      <Card style={styles.container}>
+            <Card.Content>
+            <Text
+              style={styles.title}
+            >
+              Class Schedule List
+            </Text>
+            <View style={{ flexDirection: "row", marginHorizontal: 10 }}>
+              <TextInput
+                style={styles.searchInput}
+                onChangeText={(text) => setSearchTerm(text)}
+                value={searchTerm}
+                placeholder="Search by day or module"
+                placeholderTextColor="black"
+              />
+              <TouchableOpacity
+                style={styles.searchButton}
+                onPress={() => handleSearch()}
+                underlayColor="#0084fffa"
+              >
+                <Text style={styles.searchButtonText}>Search</Text>
+              </TouchableOpacity>
+            </View>
 
-        {/* store feched data in list using react native flatlist */}
-        <FlatList
-          style={{
-            margin: 5,
-            height: "95%",
-          }}
-          data={searchTerm ? filteredData : getData}
-          renderItem={({ item }) => (
-            <View
+            
+            {/* store feched data in list using react native flatlist */}
+            <FlatList
               style={{
                 margin: 5,
-                backgroundColor: "#fff",
-                padding: 10,
-                borderRadius: 15,
-                elevation: 10,
+                height: "95%",
               }}
-            >
-              <Text style={styles.text}>Day : {item.selectedDay}</Text>
-              <Text style={styles.text}>Time : {item.selectedTime}</Text>
-              <Text style={styles.text}>Venue : {item.Venue}</Text>
-              <Text style={styles.text}>Module : {item.Module}</Text>
-              <Text style={styles.text}>Lecturer : {item.Lecturer}</Text>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "flex-end",
-                }}
-              >
-                {/* update button */}
-                <TouchableOpacity
+              data={searchTerm ? filteredData : getData}
+              renderItem={({ item }) => (
+                <View
                   style={{
-                    marginTop: 15,
-                    flex: 0.4,
-                    backgroundColor: "#0056A2",
-                    marginHorizontal: 5,
-                    height: 30,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 10,
+                    margin: 5,
+                    backgroundColor: "white",
+                    padding: 10,
+                    borderRadius: 15,
+                    elevation: 10,
                   }}
-                  activeOpacity={2}
-                  //pass data to another page using usenavigate params for update user
-                  onPress={() =>
-                    navigation.navigate("Update Schedule", { item })
-                  }
-                  underlayColor="#0084fffa"
                 >
-                  <Text style={{ fontSize: 15, color: "#fff" }}>Update</Text>
-                </TouchableOpacity>
-                {/* delete button */}
-                <TouchableOpacity
-                  style={{
-                    marginTop: 15,
-                    flex: 0.4,
-                    backgroundColor: "tomato",
-                    marginHorizontal: 5,
-                    height: 30,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 10,
-                  }}
-                  activeOpacity={2}
-                  onPress={() => deleteSchedule(item.id)}
-                  underlayColor="#0084fffa"
-                >
-                  <Text style={{ fontSize: 15, color: "#fff" }}>Delete</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          )}
-        ></FlatList>
+                  <Text style={styles.text}>Day : {item.selectedDay}</Text>
+                  <Text style={styles.text}>Time : {item.selectedTime}</Text>
+                  <Text style={styles.text}>Venue : {item.Venue}</Text>
+                  <Text style={styles.text}>Module : {item.Module}</Text>
+                  <Text style={styles.text}>Lecturer : {item.Lecturer}</Text>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: "row",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {/* update button */}
+                    <TouchableOpacity
+                      style={{
+                        marginTop: 15,
+                        flex: 0.4,
+                        backgroundColor: "#0056A2",
+                        marginHorizontal: 5,
+                        height: 30,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 10,
+                      }}
+                      activeOpacity={2}
+                      //pass data to another page using usenavigate params for update user
+                      onPress={() =>
+                        navigation.navigate("Update Schedule", { item })
+                      }
+                      underlayColor="#0084fffa"
+                    >
+                      <Text style={{ fontSize: 15, color: "#fff" }}>
+                        Update
+                      </Text>
+                    </TouchableOpacity>
+                    {/* delete button */}
+                    <TouchableOpacity
+                      style={{
+                        marginTop: 15,
+                        flex: 0.4,
+                        backgroundColor: "tomato",
+                        marginHorizontal: 5,
+                        height: 30,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 10,
+                      }}
+                      activeOpacity={2}
+                      onPress={() => deleteSchedule(item.id)}
+                      underlayColor="#0084fffa"
+                    >
+                      <Text style={{ fontSize: 15, color: "#fff" }}>
+                        Delete
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+            ></FlatList></Card.Content>
+           </Card>
+            <Toast />
       </View>
-      <Toast />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -196,6 +217,7 @@ const styles = StyleSheet.create({
     borderRadius: 7,
     paddingHorizontal: 10,
     marginRight: 10,
+    backgroundColor:'white',
   },
   searchButton: {
     backgroundColor: "#0056A2",
@@ -209,4 +231,18 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 15,
   },
+  container: {
+    alignContent: "center",
+    margin: 37,
+  },
+
+  title: {
+    fontSize: width * 0.06,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: height * 0.04,
+    marginBottom: height * 0.02,
+  },
+
+  
 });
